899906bde12664a8f7fe27bc08742019cff6d033
WaldenLu <972672562@qq.com>
2015-07-07 18:30:44 +0800

    day02

---
+++ day02/HelloHelloHello/src/com/gemptc/trihello/HelloHelloHello.java
@@ -0,0 +1,13 @@
+package com.gemptc.trihello;
+
+public class HelloHelloHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("HelloHelloHello");
+	}
+
+}
