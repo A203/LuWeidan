c8bbc7ccdee8217504c4037b10c2d0d38205c576
WaldenLu <972672562@qq.com>
2015-07-07 18:38:16 +0800

    上课练习

---
+++ Array.java
@@ -0,0 +1,17 @@
+package com.gemptc.array;
+
+public class Array {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={1,2,3,4,5};
+		//下标从0开始
+		arr[2]=8;
+        System.out.println("arr[2]="+arr[2]);
+        System.out.println(arr.length);
+	}
+
+}
